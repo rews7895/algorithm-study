24ed023331ff54f4cfd62e83b7d26e307c301d43
Junseok <yalvz@naver.com>
2022-03-04 00:05:56 +0900

    프로젝트 구조 정리 및 해시 관련 문제 작성

---
+++ __main__.py
@@ -0,0 +1,8 @@
+from library.ProgramTimer import ProgramTimer
+
+timer = ProgramTimer()
+
+timer.timerStart()
+#! Hash - 1. 완주하지 못한 선수
+from programmers.hash.not_finish_player import *
+timer.timerEnd()
---
+++ library/ProgramTimer.py
@@ -0,0 +1,16 @@
+"""
+시간 측정
+"""
+import time
+
+class ProgramTimer:
+  def __init__(self):
+    self.start = time.time()
+    self.end = 0
+
+  def timerStart(self):
+    self.start = time.time()
+
+  def timerEnd(self):
+    self.end = time.time() - self.start
+    print('실행 시간: ', round(self.end, 4), '초')
