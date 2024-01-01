fbf733b9c2dfeee0bb4c4c5f32a05a28b0c79db5
Junseok <yalvz@naver.com>
2024-01-01 22:14:15 +0900

    책 풀이 추가

---
+++ itsCodingTest/part02/chapter06/question03/question03-book01.py
@@ -0,0 +1,23 @@
+# n과 k를 입력받기
+n, k = map(int, input().split())
+# 배열 A의 모든 원소를 입력받기
+a = list(map(int, input().split()))
+# 배열 B의 모든 원소를 입력받기
+b = list(map(int, input().split()))
+
+# 배열 A는 오름차순 정렬 수행
+a.sort()
+# 배열 B는 내림차순 정렬 수행
+b.sort(reverse=True)
+
+# 첫 번째 인덱스부터 확인하며, 두 배열의 원소를 최대 K번 비교
+for i in range(k):
+    # a의 원소가 b의 원소보다 작은 경우
+    if a[i] < b[i]:
+        # 두 원소를 교체
+        a[i], b[i] = b[i], a[i]
+    else:
+        break
+
+# 배열 a의 모든 원소의 합을 출력
+print(sum(a))
