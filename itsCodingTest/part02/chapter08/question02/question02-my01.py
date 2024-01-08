6f71043973437a89080440ef19bb21e6d767420a
Junseok <yalvz@naver.com>
2024-01-08 19:36:44 +0900

    Update question02-my01.py

--- itsCodingTest/part02/chapter08/question02/question02-my01.py
+++ itsCodingTest/part02/chapter08/question02/question02-my01.py
@@ -1,10 +1,12 @@
-input = 4
-li = [1, 3, 1, 5]
-result = [0] * (input + 1)
+input = 5
+li = [3, 1, 2, 2, 5]
+result = [0] * input
 
 result[0] = li[0]
 result[1] = max(result[0], li[1])
 for i in range(2, input):
     result[i] = max(result[i - 1], result[i - 2] + li[i])
 
-print(result[input - 1])
+print(result)
+# print(result[input - 1])
+
